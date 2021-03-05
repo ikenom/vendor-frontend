15faa405385f6f9ad1796ba6e9134ac7828f558e
Robert Leonard <rleonard@ojolabs.com>
2021-03-04 19:33:12 -0500

    saving

--- src/components/organisms/Tabs/index.tsx
+++ src/components/organisms/Tabs/index.tsx
@@ -4,23 +4,24 @@ import 'antd/dist/antd.css';
 import './index.css';
 import { TabElement } from '../../atoms/tabElement';
 import styled from 'styled-components';
+import { NewActionTabContent, NewActionTabElement } from '../../molecules/needsAction';
+import { Order } from '../../../models/orders';
 
 const { TabPane } = Tabs;
 
 interface TabProps {
-
+  orders: Order[];
 }
 
 const TabPaneContainer = styled(TabPane)`
-border: "transparent";
-borderColor: "transparent";
 `;
 
 export const AppTab = (props: TabProps) => {
+  const { orders } = props;
   return(
     <Tabs >
-      <TabPaneContainer tab={<TabElement text="Needs Action" showAttentionIcon={true}/>} key="1">
-        Content of Tab Pane 1
+      <TabPaneContainer tab={<NewActionTabElement />} key="1">
+        <NewActionTabContent orders={orders} onClick={() => {}}/>
       </TabPaneContainer>
       <TabPane tab={<TabElement text="In Kitchen" showAttentionIcon={false}/>} key="2">
         Content of Tab Pane 2
@@ -28,7 +29,7 @@ export const AppTab = (props: TabProps) => {
       <TabPane tab={<TabElement text="Ready" showAttentionIcon={false}/>} key="3">
         Content of Tab Pane 3
       </TabPane>
-      <TabPane tab={<TabElement text="History" showAttentionIcon={false}/>} key="3">
+      <TabPane tab={<TabElement text="History" showAttentionIcon={false}/>} key="4">
       Content of Tab Pane 4
       </TabPane>
     </Tabs>
