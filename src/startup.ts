2dea5f64af3a3d1c6b45185f240c72f88460a00f
Ikenna Omekam <iomekam@github.com>
2021-03-27 20:36:44 -0400

    cleanup

--- src/startup.ts
+++ src/startup.ts
@@ -1,5 +1,5 @@
 import UserStore from "./store/userStore"
-import OrderStore from "./store/orderStore2"
+import OrderStore from "./store/orderStore"
 
 export const startup = async () => {
   await UserStore.init()
