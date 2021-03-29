cdc074b4f84970e1ea78191e65502ef0c6522ee6
Ikenna Omekam <iomekam@github.com>
2021-03-29 19:19:58 -0400

    fixes

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,15 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink, concat, DefaultOptions } from '@apollo/client';
 import 'cross-fetch/polyfill';
+import { createConsumer } from '@rails/actioncable'
 import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
 
 const url = process.env.BACKEND_URL
 
-let cable;
-import('@rails/actioncable').then(
-  (ActionCable) => {
-    cable = ActionCable.createConsumer(`${url}/cable`)
-  }
-)
+let cable = createConsumer(`${url}/cable`)
 
 const httpLink = createHttpLink({
   uri: `${url}/graphql`
--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -2,26 +2,18 @@ import authClient from '../api/auth_client'
 
 export default class UserStore {
 
-  public vendorId: string
-
   static init = async () => {
     const userStore = new UserStore()
-    await userStore.loginAsync("fake57@fake.com", "password")
+    await userStore.loginAsync("fake59@fake.com", "password")
   }
 
   loginAsync = async (email: String, password: String) => {
     const data = await authClient.loginAsync(email, password)
     this.saveToken(data.login.token)
-    this.vendorId = await this.getVendorId(data.login.token)
   }
 
   saveToken = (token: string) => {
     window.localStorage.setItem("token", token);
-  }
-
-  private getVendorId = async (token: string): Promise<string> => {
-    // Some query to fytr backend to get vendorId from the jwt token and return it
-    return "test-id";
   }
 }
 
