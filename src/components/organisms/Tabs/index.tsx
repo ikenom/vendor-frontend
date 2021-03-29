b3e8139bc8eb64554d3fb14ee01c5c04cb0642d0
Ikenna Omekam <iomekam@github.com>
2021-03-28 21:36:42 -0400

    remve

--- src/components/organisms/Tabs/index.tsx
+++ src/components/organisms/Tabs/index.tsx
@@ -7,7 +7,6 @@ import styled from 'styled-components';
 import { NewActionTabContent, NewActionTabElement } from '../../molecules/needsAction';
 import { Order } from '../../../models/orders';
 import { navigate } from "gatsby";
-import { useState } from '@hookstate/core';
 
 const { TabPane } = Tabs;
 
@@ -23,24 +22,18 @@ const TabPaneContainer = styled(TabPane)`
 
 export const AppTab = (props: TabProps) => {
   const { needsAction, inKitchen, ready, history } = props;
-  const activeKey = useState("1")
-
-  const isActive = (key: string): Boolean => {
-    return activeKey.get() == key
-  }
-
   return(
-    <Tabs onChange={(key) => activeKey.set(key)}>
-      <TabPane tab={<TabElement text="Needs Action" showAttentionIcon={!isActive("1") && true}/>} key="1">
-          <NewActionTabContent orders={needsAction} onClick={() => {navigate("/app/order")}}/>
-      </TabPane>
-      <TabPane tab={<TabElement text="In Kitchen" showAttentionIcon={!isActive("2")}/>} key="2">
+    <Tabs >
+      <TabPaneContainer tab={<NewActionTabElement />} key="1">
+        <NewActionTabContent orders={needsAction} onClick={() => {navigate("/app/order")}}/>
+      </TabPaneContainer>
+      <TabPane tab={<TabElement text="In Kitchen" showAttentionIcon={false}/>} key="2">
         <NewActionTabContent orders={inKitchen} onClick={() => {navigate("/app/order")}}/>
       </TabPane>
-      <TabPane tab={<TabElement text="Ready" showAttentionIcon={!isActive("3")}/>} key="3">
+      <TabPane tab={<TabElement text="Ready" showAttentionIcon={false}/>} key="3">
         <NewActionTabContent orders={ready} onClick={() => {navigate("/app/order")}}/>
       </TabPane>
-      <TabPane tab={<TabElement text="History" showAttentionIcon={!isActive("4")}/>} key="4">
+      <TabPane tab={<TabElement text="History" showAttentionIcon={false}/>} key="4">
         <NewActionTabContent orders={history} onClick={() => {navigate("/app/order")}}/>
       </TabPane>
     </Tabs>
